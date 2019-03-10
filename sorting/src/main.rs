26c600735d4d74fb8b1318b276180a629fcc2a6d
Liam Wu <liam.wu27@gmail.com>
2019-03-10 22:41:42 +0800

    selection sorting

--- sorting/src/main.rs
+++ sorting/src/main.rs
@@ -2,20 +2,52 @@ fn main() {
     println!("Rust data structure programming ~");
     println!("Bubble sorting ~\n");
 
-    let mut arr: [i32; 7] = [22, 12, 6, 88, 45, 49, 99];
+    // value of elements is different
+    let mut arr: [i32; 11] = [22, 12, 6, 88, 0, -5, 99, -1001, 12, 3, 66];
+
+    // value of elements is the same
+    //let mut arr: [i32; 11] = [6; 11];
 
     println!("Original order:");
     println!("{:?}", arr);
 
     let len = arr.len();
-    for i in 0..len {
+
+    // Bubble sorting
+    /*for i in 0..len {
+        println!("====== Round {} ======", i);
+        println!("{:?}", arr);
+
         for j in 0..(len - i - 1) {
             if arr[j] > arr[j + 1] {
+                println!("swap <j>:{} <j+1>:{}", j, j + 1);
+
                 let temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
-            println!("i:{} j:{}", i, j);
+            println!("{:?}", arr);
+        }
+    }*/
+
+    // Selection sorting
+    for i in 0..len {
+        println!("====== Round {} ======", i);
+        println!("{:?}", arr);
+        let mut min = i;
+
+        for j in i..len {
+            if arr[min] > arr[j] {
+                min = j;
+            }
+            println!("i:{} j:{} min:{} min_val:{}", i, j, min, arr[min]);
+        }
+
+        if arr[i] != arr[min] {
+            println!("swap i:{} min:{}", i, min);
+            let temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
         }
     }
 
